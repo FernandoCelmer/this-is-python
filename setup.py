42302612ce62c98e504e508db6d4cb526ff1ce98
Fernando Celmer <fernando.celmer@cakeerp.com>
2021-09-21 02:01:27 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="this-is-python-fernandocelmer",
+    version="0.0.1",
+    author="Fernando Celmer",
+    author_email="email@fernandocelmer.com",
+    description="This is Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FernandoCelmer/this-is-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
