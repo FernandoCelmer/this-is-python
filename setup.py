a72af2559c3f4b6148d3e0f9c212bd636d624976
Fernando Celmer <fernando.celmer@cakeerp.com>
2021-09-21 03:48:32 -0300

    update v0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="this-is",
-    version="0.0.6",
+    version="0.0.7",
     author="Fernando Celmer",
     author_email="email@fernandocelmer.com",
     description="This is Python",
