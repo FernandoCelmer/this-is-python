09e2cc87f1724d0378433c598c528a09e4eea06e
FernandoCelmer <email@fernandocelmer.com>
2022-04-25 23:44:28 -0300

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="this-is",
-    version="1.0.0",
+    version="1.0.1",
     author="Fernando Celmer",
     author_email="email@fernandocelmer.com",
     description="This is Zen of Python by Tim Peters",
