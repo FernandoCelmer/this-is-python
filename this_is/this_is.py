9ef429f1a48e41a2cfaad7c325f524a2c20bca7f
alberic89 <alberic89@gmx.com>
2023-02-03 15:23:35 +0100

    Add a french translation

--- this_is/this_is.py
+++ this_is/this_is.py
@@ -70,3 +70,30 @@ Si la implementación es difícil de explicar, es una mala idea.
 Si la implementación es fácil de explicar, podría ser una buena idea.
 Los espacios de nombres son una gran idea, ¡tengamos más de estos!"""
     print(msg)
+
+def fr():
+    """Traduction de Cécile Trevian, sous licence CC BY-NC-SA dans document
+    https://perso.limsi.fr/pointal/_media/python:cours:courspython3.pdf"""
+    msg = """The Zen of Python, by Tim Peters
+
+Préfère :
+      la beauté à la laideur,
+      l'explicite à l'implicite,
+      le simple au complexe
+      et le complexe au compliqué,
+      le déroulé à l'imbriqué,
+      l'aéré au compact.
+Prends en compte la lisibilité.
+Les cas particuliers ne le sont jamais assez pour violer les règles.
+Mais, à la pureté, privilégie l'aspect pratique.
+Ne passe pas les erreurs sous silence,
+... ou bâillonne-les explicitement.
+Face à l'ambiguïté, à deviner ne te laisse pas aller.
+Sache qu'il ne devrait [y] avoir qu'une et une seule façon de procéder,
+même si, de prime abord, elle n'est pas évidente, à moins d'être Néerlandais.
+Mieux vaut maintenant que jamais.
+Cependant jamais est souvent mieux qu'immédiatement.
+Si l'implémentation s'explique difficilement, c'est une mauvaise idée.
+Si l'implémentation s'explique aisément, c'est peut-être une bonne idée.
+Les espaces de nommage ! Sacrée bonne idée ! Faisons plus de trucs comme ça. """
+    print(msg)
